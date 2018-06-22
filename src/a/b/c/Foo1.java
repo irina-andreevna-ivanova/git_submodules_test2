a5a41202b70c2bab59426349bc26cec36fe35473
irina-petrovskaya <Irina.Petrovskaya@jetbrains.com>
2018-06-22 18:06:16 +0300

    Initial commit

---
+++ src/a/b/c/Foo1.java
@@ -0,0 +1,10 @@
+package a.b.c;
+
+/**
+ * *******************************
+ * Created by Irina.Petrovskaya on 6/22/2018.
+ * Project: git_submodules_test2
+ * *******************************
+ */
+public class Foo1 {
+}
